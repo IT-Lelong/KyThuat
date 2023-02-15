7f4fc4950d8e0783c5b956a98958c59a8ee5af4e
Quyen <quyen_it@mail.klb.com.tw>
2023-02-15 08:34:51 +0700

    20230215 add KT03 Login

---
+++ app/src/main/java/com/lelong/kythuat/KT03/login_kt03.java
@@ -0,0 +1,17 @@
+package com.lelong.kythuat.KT03;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.os.Bundle;
+
+import com.lelong.kythuat.R;
+
+public class login_kt03 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.kt03_activity_login);
+    }
+}
